09d210ecdbb8bc9aaeefdedcdcc97a11d1cbbf97
Mohamed Mostafa Saber <84982420+MohamedMustafaSaber@users.noreply.github.com>
2025-06-03 00:24:48 +0300

    [3355] [Time Beats: 68.99%] [Memory Beats: 17.36%] - LeetPush

---
+++ Zero-Array-Transformation-I.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public bool IsZeroArray(int[] nums, int[][] queries) {
+        int size = nums.Length;
+int[] deletionCount = new int[size + 1];
+foreach (var query in queries)
+{
+    int l = query[0], r = query[1];
+    deletionCount[l]++;
+    deletionCount[r + 1]--;
+}
+for (int i = 1; i <= size; i++)
+{
+    deletionCount[i] += deletionCount[i - 1];
+}
+for (int i = 0; i < nums.Length; i++)
+{
+    if (deletionCount[i] < nums[i])
+    {
+        return false;
+    }
+    
+}
+return true;
+    }
+}
